df396354546cf758f011ef97da97f2682dee1400
FxKu <fkunde@virtualcitysystems.de>
2015-06-06 18:30:22 +0200

    updated REVERT. Forgot to replace merge_json function. Removed functions will end up in new script.

--- REVERT.sql
+++ REVERT.sql
@@ -24,11 +24,6 @@
 * C-o-n-t-e-n-t:
 *
 * FUNCTIONS:
-*   drop_table(table_name TEXT, target_schema_name TEXT DEFAULT 'public') RETURNS SETOF VOID
-*   drop_table_relations(table_name TEXT, target_schema_name TEXT DEFAULT 'public') RETURNS SETOF VOID
-*   recreate_schema_state(schema_name TEXT, target_schema_name TEXT DEFAULT 'public', except_tables TEXT[] DEFAULT '{}') 
-*     RETURNS SETOF VOID
-*   recreate_table_state(table_name TEXT, schema_name TEXT, target_schema_name TEXT DEFAULT 'public') RETURNS SETOF VOID
 *   revert_transaction(tid BIGINT) RETURNS SETOF VOID
 ***********************************************************/
 CREATE OR REPLACE FUNCTION pgmemento.revert_transaction(tid BIGINT) RETURNS SETOF VOID AS
@@ -38,6 +33,7 @@ DECLARE
   column_name TEXT;
   delimeter VARCHAR(1);
   update_stmt TEXT;
+  updated_changes JSONB;
 BEGIN
   SET CONSTRAINTS ALL DEFERRED;
 
@@ -90,127 +86,18 @@ BEGIN
 
     -- DELETE and TRUNCATE case
     ELSE
-      r.changes := pgmemento.merge_jsonb(r.changes, json_object_agg('audit_id',nextval('pgmemento.audit_id_seq'))::jsonb);
+      EXECUTE 'WITH json_update AS
+                (SELECT * FROM jsonb_each_text($1)
+                   UNION ALL
+                 SELECT * FROM jsonb_each_text($2)
+                )
+                SELECT json_object_agg(key, value)::jsonb FROM json_update'
+                INTO updated_changes USING r.changes, json_object_agg('audit_id',nextval('pgmemento.audit_id_seq'))::jsonb;
+
       EXECUTE format('INSERT INTO %I.%I
                         SELECT * FROM jsonb_populate_record(null::%I.%I, %L)',
-                        r.schema_name, r.table_name, r.schema_name, r.table_name, r.changes);
+                        r.schema_name, r.table_name, r.schema_name, r.table_name, updated_changes);
     END IF;
   END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
-
-/**********************************************************
-* RECREATE SCHEMA STATE
-*
-* If a schema state shall be recreated as the actual database
-* the recent tables are truncated and dropped first and the
-* the former state is rebuild from the schema that contains
-* the former state.
-*
-* NOTE: In order to rebuild primary keys, foreign keys and 
-*       indexes corresponding functions must have been executed
-*       on target schema.
-***********************************************************/
--- drop foreign key contraints
-CREATE OR REPLACE FUNCTION pgmemento.drop_table_relations(
-  table_name TEXT,
-  target_schema_name TEXT DEFAULT 'public'
-  ) RETURNS SETOF VOID AS
-$$
-DECLARE
-  fkey TEXT;
-BEGIN
-  FOR fkey IN EXECUTE 'SELECT constraint_name AS fkey_name FROM information_schema.table_constraints 
-                         WHERE constraint_type = ''FOREIGN KEY'' AND table_schema = $1 AND table_name= $2'
-                          USING target_schema_name, table_name LOOP
-    EXECUTE format('ALTER TABLE %I.%I DROP CONSTRAINT %I', target_schema_name, table_name, fkey);
-  END LOOP;
-END;
-$$
-LANGUAGE plpgsql;
-
--- truncate and drop table and all depending objects
-CREATE OR REPLACE FUNCTION pgmemento.drop_table(
-  table_name TEXT,
-  target_schema_name TEXT DEFAULT 'public'
-  ) RETURNS SETOF VOID AS
-$$
-BEGIN
-  -- trigger the log_truncate_trigger
-  EXECUTE format('TRUNCATE TABLE %I.%I', target_schema_name, table_name);
-
-  -- dropping the table
-  EXECUTE format('DROP TABLE %I.%I CASCADE', target_schema_name, table_name);
-END;
-$$
-LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION pgmemento.recreate_schema_state(
-  schema_name TEXT,
-  target_schema_name TEXT DEFAULT 'public',
-  except_tables TEXT[] DEFAULT '{}'
-  ) RETURNS SETOF VOID AS
-$$
-BEGIN
-  -- drop foreign keys in target schema
-  EXECUTE 'SELECT pgmemento.drop_table_relations(tablename, schemaname) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables;
-
-  -- drop tables in target schema
-  EXECUTE 'SELECT pgmemento.drop_table(tablename, schemaname) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables;
-
-  -- copy tables of chosen schema into target schema
-  EXECUTE 'SELECT pgmemento.recreate_table_state(tablename, schemaname, $3) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING schema_name, except_tables, target_schema_name;
-
-  -- create primary keys for tables in target schema
-  EXECUTE 'SELECT pgmemento.pkey_table_state(tablename, schemaname, $3) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables, schema_name;
-
-  -- create foreign keys for tables in target schema
-  EXECUTE 'SELECT pgmemento.fkey_table_state(tablename, schemaname, $3) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables, schema_name;
-
-  -- index tables in target schema
-  EXECUTE 'SELECT pgmemento.index_table_state(tablename, schemaname, $3) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables, schema_name;
-
-  -- add default values for tables in target schema
-  EXECUTE 'SELECT pgmemento.default_values_table_state(tablename, schemaname, $3) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables, schema_name;
-
-  -- activate loggin triggers in target schema 
-  EXECUTE 'SELECT pgmemento.create_table_log_trigger(tablename, schemaname) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables;
-
-  -- fill pgmemento_log table with entries from new tables in target schema
-  EXECUTE 'SELECT pgmemento.log_table_state(tablename, schemaname) FROM pg_tables 
-             WHERE schemaname = $1 AND tablename <> ALL ($2)' 
-             USING target_schema_name, except_tables;
-END;
-$$
-LANGUAGE plpgsql;
-
--- recreate table state into the schema used as the recent database state
-CREATE OR REPLACE FUNCTION pgmemento.recreate_table_state(
-  table_name TEXT,
-  schema_name TEXT,
-  target_schema_name TEXT DEFAULT 'public'
-  ) RETURNS SETOF VOID AS
-$$
-BEGIN
-  EXECUTE format('CREATE TABLE %I.%I AS SELECT * FROM %I.%I', target_schema_name, table_name, schema_name, table_name);
-END;
-$$
-LANGUAGE plpgsql;
