ea257bbf5fe9000aa136985f7609795d44280698
FxKu <felix-kunde@gmx.de>
2016-02-15 17:14:47 +0100

    changed API for LOG_UTIL functions (incl. minor fixes)

--- src/LOG_UTIL.sql
+++ src/LOG_UTIL.sql
@@ -1,6 +1,6 @@
 -- LOG_UTIL.sql
 --
--- Author:      Felix Kunde <fkunde@virtualcitysystems.de>
+-- Author:      Felix Kunde <felix-kunde@gmx.de>
 --
 --              This script is free software under the LGPL Version 3
 --              See the GNU Lesser General Public License at
@@ -15,15 +15,20 @@
 -- ChangeLog:
 --
 -- Version | Date       | Description                                 | Author
--- 0.3.0     2015-06-20   initial commit                                FKun
+-- 0.2.0     2016-02-15   get txids done right                          FKun
+-- 0.1.0     2015-06-20   initial commit                                FKun
 --
 
 /**********************************************************
 * C-o-n-t-e-n-t:
 *
 * FUNCTIONS:
-*   get_transaction_id(aid BIGINT) RETURNS BIGINT
-*   get_transaction_id_bounds(table_name TEXT, schema_name TEXT DEFAULT 'public'::text, OUT txid_min BIGINT, OUT txid_max BIGINT) RETURNS RECORD
+*   delete_table_event_log(t_id BIGINT, t_name TEXT, s_name TEXT DEFAULT 'public'::text) RETURNS SETOF INTEGER
+*   delete_txid_log(t_id BIGINT) RETURNS BIGINT
+*   get_max_txid_to_audit_id(aid BIGINT) RETURNS BIGINT
+*   get_min_txid_to_audit_id(aid BIGINT) RETURNS BIGINT
+*   get_txid_bounds_to_table(table_name TEXT, schema_name TEXT DEFAULT 'public', OUT txid_min BIGINT, OUT txid_max BIGINT) RETURNS RECORD
+*   get_txids_to_audit_id(aid BIGINT) RETURNS SETOF BIGINT
 *
 * VIEW:
 *   audit_tables
@@ -36,17 +41,34 @@
 * Simple functions to return the transaction_id related to
 * certain database entities 
 ***********************************************************/
-CREATE OR REPLACE FUNCTION pgmemento.get_transaction_id(aid BIGINT) RETURNS BIGINT AS
+CREATE OR REPLACE FUNCTION pgmemento.get_txids_to_audit_id(aid BIGINT) RETURNS SETOF BIGINT AS
 $$
   SELECT t.txid FROM pgmemento.transaction_log t
-             JOIN pgmemento.table_event_log e ON e.transaction_id = t.txid
-             JOIN pgmemento.row_log r ON r.event_id = e.id
-             WHERE r.id = aid;
+    JOIN pgmemento.table_event_log e ON e.transaction_id = t.txid
+    JOIN pgmemento.row_log r ON r.event_id = e.id
+      WHERE r.audit_id = aid;
 $$
 LANGUAGE sql;
 
+CREATE OR REPLACE FUNCTION pgmemento.get_min_txid_to_audit_id(aid BIGINT) RETURNS BIGINT AS
+$$
+  SELECT min(t.txid) FROM pgmemento.transaction_log t
+    JOIN pgmemento.table_event_log e ON e.transaction_id = t.txid
+    JOIN pgmemento.row_log r ON r.event_id = e.id
+      WHERE r.audit_id = aid;
+$$
+LANGUAGE sql;
 
-CREATE OR REPLACE FUNCTION pgmemento.get_transaction_id_bounds(
+CREATE OR REPLACE FUNCTION pgmemento.get_max_txid_to_audit_id(aid BIGINT) RETURNS BIGINT AS
+$$
+  SELECT max(t.txid) FROM pgmemento.transaction_log t
+    JOIN pgmemento.table_event_log e ON e.transaction_id = t.txid
+    JOIN pgmemento.row_log r ON r.event_id = e.id
+      WHERE r.audit_id = aid;
+$$
+LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION pgmemento.get_txid_bounds_to_table(
   table_name TEXT,
   schema_name TEXT DEFAULT 'public'::text,
   OUT txid_min BIGINT,
@@ -54,7 +76,10 @@ CREATE OR REPLACE FUNCTION pgmemento.get_transaction_id_bounds(
   ) RETURNS RECORD AS
 $$
 BEGIN
-  EXECUTE format('SELECT pgmemento.get_transaction_id(min(audit_id)) AS tmin, pgmemento.get_transaction_id(max(audit_id)) AS tmax FROM %I.%I', schema_name, table_name) INTO txid_min, txid_max;
+  EXECUTE format(
+    'SELECT pgmemento.get_min_txid_to_audit_id(min(audit_id)) AS tmin, 
+            pgmemento.get_max_txid_to_audit_id(max(audit_id)) AS tmax 
+     FROM %I.%I', schema_name, table_name) INTO txid_min, txid_max;
   RETURN;
   
   EXCEPTION
@@ -84,7 +109,7 @@ CREATE OR REPLACE FUNCTION pgmemento.delete_table_event_log(
   ) RETURNS SETOF INTEGER AS
 $$
 BEGIN
-  RETURN QUERY 
+  RETURN QUERY
     DELETE FROM pgmemento.table_event_log 
       WHERE transaction_id = t_id
         AND schema_name = s_name
@@ -94,22 +119,24 @@ $$
 LANGUAGE plpgsql;
 
 
-
 /***********************************************************
-CREATE VIEW
-
+* CREATE VIEW
+*
+* A view that shows the user at which transaction auditing
+* has been started. This will be useful when switch the
+* production schema. (more functions to come in the near future)
 ***********************************************************/
 CREATE OR REPLACE VIEW pgmemento.audit_tables AS
   SELECT
     t.schemaname, t.tablename, b.txid_min, b.txid_max 
   FROM pg_class c, pg_namespace n, pg_tables t, pg_attribute a
     JOIN LATERAL (
-      SELECT * FROM pgmemento.get_transaction_id_bounds(t.tablename, t.schemaname)
+      SELECT * FROM pgmemento.get_txid_bounds_to_table(t.tablename, t.schemaname)
     ) b ON (true)
   WHERE c.relname = t.tablename
     AND n.oid = c.relnamespace 
     AND n.nspname = t.schemaname 
     AND a.attrelid = c.oid
-    AND a.attname = 'audit_id' 
     AND t.schemaname != 'pgmemento'
+    AND a.attname = 'audit_id' 
     ORDER BY schemaname, tablename;
