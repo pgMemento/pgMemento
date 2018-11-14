2e54029dca1d208ee00e200cbca392f96d235f3d
Felix Kunde <felix-kunde@gmx.de>
2018-11-14 22:11:33 +0100

    added test for restoring a relational layout using jsonb_populate_record and a restore_template

--- test/restore/TEST_RESTORE_RECORD.sql
+++ test/restore/TEST_RESTORE_RECORD.sql
@@ -14,6 +14,7 @@
 -- ChangeLog:
 --
 -- Version | Date       | Description                                    | Author
+-- 0.2.0     2018-11-14   added test with restore_template                 FKun
 -- 0.1.0     2018-10-28   initial commit                                   FKun
 --
 
@@ -37,7 +38,7 @@ BEGIN
 
   ASSERT column_list = 'AS (id integer, lineage text, audit_id bigint)', 'Incorrect column definition list: %', column_list;
   
-  -- save column_list for next tests
+  -- save column_list for next test
   PERFORM set_config('pgmemento.column_list', column_list, FALSE);
 END
 $$
@@ -50,7 +51,6 @@ $$
 DECLARE
   query_sring TEXT := 'SELECT * FROM pgmemento.restore_record(1, 16, ''object'', ''public'', 3)';
   rec RECORD;
-  jsonb_log JSONB;
 BEGIN
   -- append saved column list to query string
   query_sring := query_sring || current_setting('pgmemento.column_list');
@@ -59,7 +59,18 @@ BEGIN
 
   ASSERT rec.id = 2, 'Incorrect historic value for ''id'' column. Expected 2, but found %', rec.id;
   ASSERT rec.lineage = 'pgm_upsert_test', 'Incorrect historic value for ''lineage'' column. Expected ''pgm_upsert_test'', but found %', rec.lineage;
+END;
+$$
+LANGUAGE plpgsql;
 
+
+\echo
+\echo 'TEST ':n'.3: Restore single row as JSONB'
+DO
+$$
+DECLARE
+  jsonb_log JSONB;
+BEGIN
   -- restore row as JSONB
   SELECT
     *
@@ -69,7 +80,35 @@ BEGIN
     pgmemento.restore_record(1, 16, 'object', 'public', 3, TRUE)
     AS (log JSONB);
 
+  -- save jonsb log for next tests
+  PERFORM set_config('pgmemento.restore_record_3_jsonb_log', jsonb_log::text, FALSE);
+
   ASSERT jsonb_log = '{"id": 2, "lineage": "pgm_upsert_test", "audit_id": 3}'::jsonb, 'Incorrect historic record. Expected JSON ''{"id": 2, "lineage": "pgm_upsert_test", "audit_id": 3}'', but found %', jsonb_log;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+\echo
+\echo 'TEST ':n'.4: Restore single row through JSONB and template'
+DO
+$$
+DECLARE
+  rec RECORD;
+BEGIN
+  -- create a template to be used for jsonb_populate_record
+  PERFORM pgmemento.create_restore_template(16, 'object_tmp', 'object', 'public', FALSE);
+
+  -- restore row as JSONB
+  SELECT
+    *
+  INTO
+    rec
+  FROM
+    jsonb_populate_record(null::object_tmp, current_setting('pgmemento.restore_record_3_jsonb_log')::jsonb);
+
+  ASSERT rec.id = 2, 'Incorrect historic value for ''id'' column. Expected 2, but found %', rec.id;
+  ASSERT rec.lineage = 'pgm_upsert_test', 'Incorrect historic value for ''lineage'' column. Expected ''pgm_upsert_test'', but found %', rec.lineage;
 END;
 $$
 LANGUAGE plpgsql;
