966ad635f0a82297e7c34b47b4a3b793ca031f1b
dnsl48 <dnsl48@gmail.com>
2019-03-06 07:57:31 +1300

    Extension | Split PG versions on travis

--- extension/pgxn/tests/run.sh
+++ extension/pgxn/tests/run.sh
@@ -4,33 +4,13 @@ set -e
 
 cd $(dirname "$0");
 
-NO_BACKUP_RECOVERY=$1
+PG_VERSION=$1
+NO_BACKUP_RECOVERY=$2
 
 TESTCMD="docker exec -it --env NO_BACKUP_RECOVERY=\$NO_BACKUP_RECOVERY \$CONTAINER bash -c 'cd /home/pgmemento/extension/pgxn/tests && /bin/bash install.sh && /bin/bash ../../tests/testrun.sh';"
 
-docker build -t pgmemento-test--11 - < ../../tests/Dockerfile-11;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../../..:/home/pgmemento pgmemento-test--11);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--10 - < ../../tests/Dockerfile-10;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../../..:/home/pgmemento pgmemento-test--10);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--96 - < ../../tests/Dockerfile-96;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../../..:/home/pgmemento pgmemento-test--96);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--95 - < ../../tests/Dockerfile-95;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../../..:/home/pgmemento pgmemento-test--95);
+docker build -t pgmemento-test--$PG_VERSION - < ../../tests/Dockerfile-$PG_VERSION;
+CONTAINER=$(docker run --rm -d -v $(pwd)/../../..:/home/pgmemento pgmemento-test--$PG_VERSION);
 sleep 5;
 eval $TESTCMD
 docker stop $CONTAINER
--- extension/tests/run.sh
+++ extension/tests/run.sh
@@ -4,33 +4,13 @@ set -e
 
 cd $(dirname "$0");
 
-NO_BACKUP_RECOVERY=$1
+PG_VERSION=$1
+NO_BACKUP_RECOVERY=$2
 
 TESTCMD="docker exec -it --env NO_BACKUP_RECOVERY=\$NO_BACKUP_RECOVERY \$CONTAINER bash -c 'cd /home/pgmemento/extension/tests && /bin/bash install.sh && /bin/bash testrun.sh';"
 
-docker build -t pgmemento-test--11 - < ./Dockerfile-11;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../..:/home/pgmemento pgmemento-test--11);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--10 - < ./Dockerfile-10;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../..:/home/pgmemento pgmemento-test--10);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--96 - < ./Dockerfile-96;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../..:/home/pgmemento pgmemento-test--96);
-sleep 5;
-eval $TESTCMD
-docker stop $CONTAINER
-
-
-docker build -t pgmemento-test--95 - < ./Dockerfile-95;
-CONTAINER=$(docker run --rm -d -v $(pwd)/../..:/home/pgmemento pgmemento-test--95);
+docker build -t pgmemento-test--$PG_VERSION - < ./Dockerfile-$PG_VERSION;
+CONTAINER=$(docker run --rm -d -v $(pwd)/../..:/home/pgmemento pgmemento-test--$PG_VERSION);
 sleep 5;
 eval $TESTCMD
 docker stop $CONTAINER
