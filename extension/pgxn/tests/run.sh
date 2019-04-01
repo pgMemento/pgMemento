b3d11980fd3922bffcee1c6f5e40edc5cc993d8b
dnsl48 <dnsl48@gmail.com>
2019-04-02 06:19:43 +1300

    TravisCI / Fix container parameters pass

--- extension/pgxn/tests/run.sh
+++ extension/pgxn/tests/run.sh
@@ -9,4 +9,4 @@ NO_BACKUP_RECOVERY=$2
 
 TESTCMD="docker exec -it --env NO_BACKUP_RECOVERY=\$NO_BACKUP_RECOVERY \$CONTAINER bash -c 'cd /home/pgmemento/extension/pgxn/tests && /bin/bash install.sh && /bin/bash ../../tests/testrun.sh';"
 
-/bin/bash ../../tests/container.sh $PG_VERSION $NO_BACKUP_RECOVERY $TESTCMD
+/bin/bash ../../tests/container.sh "$PG_VERSION" "$NO_BACKUP_RECOVERY" "$TESTCMD"
