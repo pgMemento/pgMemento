fda718d73c879092e9f25e4efb5ff7a379e35f13
Felix Kunde <felix-kunde@gmx.de>
2019-07-01 00:55:56 +0200

    fix run.sh scripts

--- extension/tests/run.sh
+++ extension/tests/run.sh
@@ -6,6 +6,6 @@ cd $(dirname "$0");
 
 PG_VERSION=$1
 
-TESTCMD="docker exec -it --env \$CONTAINER bash -c 'cd /home/pgmemento/extension/tests && /bin/bash install.sh && /bin/bash testrun.sh';"
+TESTCMD="docker exec -it \$CONTAINER bash -c 'cd /home/pgmemento/extension/tests && /bin/bash install.sh && /bin/bash testrun.sh';"
 
 /bin/bash ./container.sh "$PG_VERSION" "$TESTCMD"
