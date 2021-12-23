e67ac1d51d3daa92fccbcfd92c5a06d408fb1b99
Felix Kunde <felix-kunde@gmx.de>
2021-12-23 16:26:00 +0100

    provide update path from 0.7.1 to 0.7.2 with ALTER EXTENSION (#89)
    * provide update path from 0.7.1 to 0.7.2 with ALTER EXTENSION
    * update build no in docs

--- extension/pgxn/build.sh
+++ extension/pgxn/build.sh
@@ -20,6 +20,7 @@ echo "done";
 echo -n "Compiling the extension SQL ... ";
 ../compile.sh > $EXTFOLDER/sql/pgmemento.sql ;
 cp ../../UPGRADE_v07_to_v072.sql $EXTFOLDER/sql/pgmemento--0.7--$VERSION.sql ;
+cp ../../UPGRADE_v07_to_v072.sql $EXTFOLDER/sql/pgmemento--0.7.1--$VERSION.sql ;
 cp ../pgmemento.control $EXTFOLDER/. ;
 cp Makefile $EXTFOLDER/. ;
 cp META.json $EXTFOLDER/. ;
